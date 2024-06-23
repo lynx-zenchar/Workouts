e37b71a3362dbb16a8ebce1c439576c195c52150
Matthew Eleazar <meleazar@nd.edu>
2024-06-22 22:38:50 -0400

    see less

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -2,10 +2,13 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import exercises from '../../assets/data/exercises.json';
 import {Stack} from 'expo-router';
+import { useState } from 'react';
 
 
 export default function ExerciseDetailsScreen(){
     const params = useLocalSearchParams();
+
+    const [isInstructionExpanded, setIsInstructionExpanded] = useState(false);
 
     const exercise = exercises.find(item => item.name = params.name);
 
@@ -29,7 +32,10 @@ export default function ExerciseDetailsScreen(){
             </View>
 
             <View style={styles.panel}>
-              <Text style={styles.instructions}> {exercise.instructions} </Text>
+              <Text style={styles.instructions} numberOfLines={isInstructionExpanded ? 0 : 3}> 
+              {exercise.instructions} </Text>
+              <Text onPress={() =>setIsInstructionExpanded(!isInstructionExpanded)} 
+              style={styles.seeMore}> {isInstructionExpanded ? 'See less' : 'See more'} </Text>
             </View>
             
         </ScrollView>
@@ -62,7 +68,13 @@ const styles = StyleSheet.create({
   instructions: {
     fontSize: 16,
     lineHeight: 22,
-  }
+  },
+  seeMore:{
+    alignSelf: 'center',
+    padding: 5,
+    fontWeight: '600',
+    color: 'gray',
+  },
 
 
 })
